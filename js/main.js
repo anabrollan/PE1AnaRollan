1e0f34d65ae70980ca9f4bfcdfd98c71347e50e8
anabrollan <anabrollan@gmail.com>
2024-07-10 17:12:45 -0400

    Agrego foto de fondo en css

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-
-
 let newBook = prompt("Ingrese un título y autor/-a");
 let finish = "salir";
 
-while (newBook !== finish) { 
+while (newBook.toLowerCase() !== finish.toLowerCase()) { 
+
   if (newBook) {
     alert("Tu libro se ha ingresado correctamente");
     console.log("Este año has leído " + newBook);
     newBook = prompt("Ingrese otro libro o escriba 'salir' para terminar");
+
   } else {
     alert("Por favor, introduce un título y autor");
     console.log("No has introducido un título y autor/-a");
