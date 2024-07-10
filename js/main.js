5364df9a0c4bfb4b4b5036f1790317f841015a60
anabrollan <anabrollan@gmail.com>
2024-07-10 16:47:19 -0400

    Agrego el ciclo con while

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-let newBook = prompt("Ingrese un título y autor/-a");
-    console.log ("Tus libros leídos este año son: " + newBook);
 
-if (newBook == "salir"){ 
-    console.log("No hay ningún libro en tu lista");
+
+let newBook = prompt("Ingrese un título y autor/-a");
+let finish = "salir";
+
+while (newBook !== finish) { 
+  if (newBook) {
+    alert("Tu libro se ha ingresado correctamente");
+    console.log("Este año has leído " + newBook);
+    newBook = prompt("Ingrese otro libro o escriba 'salir' para terminar");
+  } else {
+    alert("Por favor, introduce un título y autor");
+    console.log("No has introducido un título y autor/-a");
+    newBook = prompt("Ingrese un título y autor/-a");
+  }
 }
+
+alert("Has terminado de ingresar tu lista de libros"); 
