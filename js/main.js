dfdb9e8c63455b7911764a5b7086b05a503f8ad9
anabrollan <anabrollan@gmail.com>
2024-07-10 11:03:02 -0400

    Agrego una condicional

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 let newBook = prompt("Ingrese un título y autor/-a");
-console.log ("Tus libros leídos este año son: " + newBook);
+    console.log ("Tus libros leídos este año son: " + newBook);
+
+if (newBook == "salir"){ 
+    console.log("No hay ningún libro en tu lista");
+}
