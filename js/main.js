1f9981f7c98ef23f558ea08d632d07e6f9bf8aad
anabrollan <anabrollan@gmail.com>
2024-07-11 05:10:34 -0400

    Agrego la función de bienvenida

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-let newBook = prompt("Ingrese un título y autor/-a");
+function welcome(){
+  alert("Te doy la bienvenida a esta aplicación de seguimiento de lecturas");
+}
+
+welcome();
+
+let newBook = prompt("Ingresa un título y autor/-a");
 let finish = "salir";
 
 if (newBook == null) {
@@ -11,12 +17,12 @@ while (newBook.toLowerCase() !== finish.toLowerCase()) {
   if (newBook) {
     alert("Tu libro se ha ingresado correctamente");
     console.log("Este año has leído " + newBook);
-    newBook = prompt("Ingrese otro libro o escriba 'salir' para terminar");
+    newBook = prompt("Ingresa otro libro o escribe 'salir' para terminar");
 
   } else {
     alert("Por favor, introduce un título y autor");
     console.log("No has introducido un título y autor/-a");
-    newBook = prompt("Ingrese un título y autor/-a");
+    newBook = prompt("Ingresa un título y autor/-a");
   }
 }
 
